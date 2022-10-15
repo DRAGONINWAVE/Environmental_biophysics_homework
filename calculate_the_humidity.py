dc12b6843e227420377c8cc02cf1d07505aa141b
DRAGONINWAVE <yl597591@gmail.com>
2022-10-15 21:17:17 +0800

    calculate_the_humidity

---
+++ calculate_the_humidity.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+data = pd.read_excel(
+    r"D:\\Environmental_biophysics_homework\\家庭作业2—大气湿度的计算\\hw1_data_20211026.xlsx")
+print(data)
