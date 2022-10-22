d5ce2f7b1d313f63e2948d6e2fbf323a0fe18197
DRAGONINWAVE <yl597591@gmail.com>
2022-10-22 22:21:29 +0800

    abandon

--- environmental_biophysics_homewor2R/compute_all_monthly.R
+++ environmental_biophysics_homewor2R/compute_all_monthly.R
@@ -5,5 +5,18 @@ library(dplyr)
 
 data0 <- read_excel("D:\\Environmental_biophysics_homework\\DAY.xlsx")
 print(data0)
-es <- data0["Ta"]
-print(es)
+# print(exp(2))
+es <- 0.611 * exp((data0["Ta"] * 17.502) / (data0["Ta"] + 240.97)) * 10
+ea <- es * data0["RH"] / 100
+data0["es"] <- es
+data0["ea"] <- ea
+VPD <- es - ea
+data0["VPD"] <- VPD
+qa <- 0.622 * (ea / (data0["P"] - 0.378 * ea)) * 1000
+data0["qa"] <- qa
+rou_v <- 0.622 * ea * 100 / (287.05 * (data0["Ta"] + 273.15)) * 1000
+data0["rou_v"] <- rou_v
+Cv <- ea / data0["P"] * 1000000
+data0["Cv"] <- round(Cv, digits = 0)
+print(data0)
+write_xlsx(data0, "D:\\Environmental_biophysics_homework\\environmental_biophysics_homewor2R\\ALL_BY_Date.xlsx")
